529524637c0a8b9e069246d3e0048cb0ece3ad59
caglayan <caglayanustaa@gmail.com>
2025-10-14 00:19:46 +0300

    login page and login form done css left

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,25 +1,26 @@
-import { Suspense } from 'react'
+import { Suspense,lazy } from 'react'
 import styles from './App.module.css'
 import { Routes, Route } from 'react-router-dom'
 import DashboardPage from '../../pages/DashBoardPage'
-import LoginPage from '../../pages/LoginPage'
 import RegistationPage from '../../pages/RegistationPage'
 import RestrictedRoute from '../RestrictedRoute'
 import StatisticsTab from '../../pages/StatisticsTab'
 import CurrencyTab from '../CurrencyTab/CurrencyTab'
 import PrivateRoute from '../PrivateRoute'
 
+const LoginPage = lazy(()=> import("../../pages/LoginPage"))
+
 function App() {
 
 
   return (
     <div className={styles.App}>
-      <Suspense>
+      <Suspense fallback={null}>
         <Routes>
           <Route path="/" element={<PrivateRoute redirectTo="/login"><DashboardPage /></PrivateRoute>}>
           </Route>
-          <Route path="/login" element={<RestrictedRoute redirectTo="/"><LoginPage /></RestrictedRoute>}>
-          </Route>
+          <Route path="/login" index element={<RestrictedRoute redirectTo="/dashboard" component={<LoginPage/>} />} />
+          
           <Route path="/register" element={<RestrictedRoute redirectTo="/"><RegistationPage /></RestrictedRoute>}>
           </Route>
           <Route path="/statistics" element={<StatisticsTab />} />
--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,10 +1,51 @@
-import React from 'react'
+import css from "./LoginForm.module.css"
+import { logIn } from "../../redux/auth/operations";
+import * as Yup from "yup";
+import { Formik, Field, Form,ErrorMessage } from 'formik';
+import { useId } from "react";
+import { useDispatch } from "react-redux";
+
+const initialValues = {
+    email: "",
+    password:""
+}
+const FeedBackSchema = Yup.object().shape({
+    email: Yup.string().email("Must be a valid email!").required("Required"),
+    password:Yup.string().min(6,"Please enter 6 more characters").max(12,"Please do not exceed 12 characters.").required("Required")
+})
 
 const LoginForm = () => {
+    const dispatch = useDispatch()
+
+    const emailField = useId()
+    const passwordField = useId()
+
+    const handleSubmit = (values, actions) => {
+        
+        dispatch(logIn(values))
+
+
+        actions.resetForm()
+    }
+
+
     return (
-        <div>
-            <p>Login Form</p>
-        </div>
+        <>
+            <h1>Money Guard</h1>
+            <Formik initialValues={initialValues} onSubmit={handleSubmit} validationSchema={FeedBackSchema}>
+                <Form className={css.form}>
+                    <label htmlFor={emailField}></label>
+                    <Field type="email" id={emailField} name="email"></Field>
+                    <ErrorMessage name="email" component="span" />
+
+                    <label htmlFor={passwordField}></label>
+                    <Field type="password" id={passwordField} name="password"></Field>
+                    <ErrorMessage name="password" component="span" />
+
+                    <button type="submit">LOG IN</button>
+                </Form>
+            </Formik>
+        </>
     )
 }
 
