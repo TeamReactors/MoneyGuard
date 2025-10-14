c2b2724793f63b4394b1ca31b81bc9dffede5893
yesimbozkurt <yesimsbozkurt@gmail.com>
2025-10-14 21:09:02 +0300

    fix: improve route handling and formatting in App component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,6 @@
-import { Suspense,lazy } from 'react'
+import { Suspense, lazy } from 'react'
 import styles from './App.module.css'
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
 import DashboardPage from '../../pages/DashBoardPage'
 import RegistationPage from '../../pages/RegistationPage'
 import RestrictedRoute from '../RestrictedRoute'
@@ -8,7 +8,7 @@ import StatisticsTab from '../../pages/StatisticsTab'
 import CurrencyTab from '../CurrencyTab/CurrencyTab'
 import PrivateRoute from '../PrivateRoute'
 
-const LoginPage = lazy(()=> import("../../pages/LoginPage"))
+const LoginPage = lazy(() => import("../../pages/LoginPage"))
 
 function App() {
 
@@ -17,14 +17,11 @@ function App() {
     <div className={styles.App}>
       <Suspense fallback={null}>
         <Routes>
-          <Route path="/" element={<PrivateRoute redirectTo="/login"><DashboardPage /></PrivateRoute>}>
-          </Route>
-          <Route path="/login" index element={<RestrictedRoute redirectTo="/dashboard" component={<LoginPage/>} />} />
-          
-          <Route path="/register" element={<RestrictedRoute redirectTo="/"><RegistationPage /></RestrictedRoute>}>
-          </Route>
+          <Route path="/" element={<Navigate to="/login" />} />
+          <Route path="/login" element={<RestrictedRoute redirectTo="/dashboard" component={<LoginPage />} />} />
+          <Route path="/register" element={<RestrictedRoute redirectTo="/"><RegistationPage /></RestrictedRoute>} />
           <Route path="/statistics" element={<StatisticsTab />} />
-          <Route path="/dashboard" element={<DashboardPage />} />
+          <Route path="/dashboard" element={<PrivateRoute redirectTo="/login"><DashboardPage /></PrivateRoute>} />
           <Route path="/currency" element={<CurrencyTab />} />
           <Route path="*" element={<div>404 Not Found</div>} />
         </Routes>
