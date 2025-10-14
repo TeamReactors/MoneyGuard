d33c161305ce0c2a5010f68a9bb6401f699bd5d1
Ozan Can CUYAR <ozncncyr@outlook.com>
2025-10-14 16:10:40 +0300

    refactor: improve code formatting and consistency in auth operations

--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -3,22 +3,33 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 
 axios.defaults.baseURL = "https://wallet.b.goit.study";
 
-
 const setAuthHeader = (token) => {
-    axios.defaults.headers.common.Authorization = `Bearer ${token}`
-}
+  axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+};
 
 const clearAuthHeader = () => {
-    axios.defaults.headers.common.Authorization = ""
-}
+  axios.defaults.headers.common.Authorization = "";
+};
 
-export const logIn = createAsyncThunk("auth/login", async (credentials,thunkAPI) => {
+export const logIn = createAsyncThunk(
+  "auth/login",
+  async (credentials, thunkAPI) => {
     try {
-        const res = await axios.post("/api/auth/sign-in", credentials);
+      const res = await axios.post("/api/auth/sign-in", credentials);
 
-        setAuthHeader(res.data.token)
-        return res.data
+      setAuthHeader(res.data.token);
+      return res.data;
     } catch (error) {
-        return thunkAPI.rejectWithValue(error.message)
+      return thunkAPI.rejectWithValue(error.message);
     }
-})
+  }
+);
+
+export const logOut = createAsyncThunk("auth/logout", async (_, thunkAPI) => {
+  try {
+    await axios.post("/api/auth/sign-out");
+    clearAuthHeader();
+  } catch (error) {
+    return thunkAPI.rejectWithValue(error.message);
+  }
+});
