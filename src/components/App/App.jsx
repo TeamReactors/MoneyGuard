9f8ed2762e29e02f045e9f621c7868be61d8fb31
caglayan <caglayanustaa@gmail.com>
2025-10-15 20:04:57 +0300

    integration refresh token and done private route

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,4 @@
-import { Suspense, lazy } from 'react'
+import { Suspense, lazy, useEffect } from 'react'
 import styles from './App.module.css'
 import { Routes, Route, Navigate } from 'react-router-dom'
 import DashboardPage from '../../pages/DashboardPage'
@@ -9,11 +9,24 @@ import CurrencyTab from '../CurrencyTab/CurrencyTab'
 import PrivateRoute from '../PrivateRoute'
 import Header from '../Header/Header'
 import Navigation from '../Navigation/Navigation'
+import { useDispatch, useSelector } from 'react-redux'
+import { refreshUser } from '../../redux/auth/operations'
+import { selectIsRefreshing } from '../../redux/auth/selectors'
 
 const LoginPage = lazy(() => import("../../pages/LoginPage"))
 
 function App() {
-  return (
+  const dispatch = useDispatch()
+  const isRefreshing = useSelector(selectIsRefreshing)
+
+  useEffect(() => {
+    dispatch(refreshUser())
+  },[dispatch])
+
+  return isRefreshing ? (
+    <strong>Refreshing user...</strong>
+  ) : (
+
     <div className={styles.App}>
       <Suspense fallback={null}>
         <Routes>
@@ -22,7 +35,7 @@ function App() {
           <Route path="/login" element={<RestrictedRoute redirectTo="/dashboard" component={<LoginPage />} />} />
           <Route path="/register" element={<RestrictedRoute redirectTo="/dashboard" component={<RegistationPage />} />} />
           <Route path="/statistics" element={<StatisticsTab />} />
-          <Route path="/dashboard" element={<PrivateRoute redirectTo="/login"><DashboardPage /></PrivateRoute>} />
+          <Route path="/dashboard" element={<PrivateRoute redirectTo="/login" component={<DashboardPage />} />} />
           <Route path="/currency" element={<CurrencyTab />} />
            <Route path="/header" element={<Header />} />
           <Route path="*" element={<div>404 Not Found</div>} />
