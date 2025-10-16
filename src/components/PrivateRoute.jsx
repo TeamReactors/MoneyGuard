9212d60409a3e4d723440e2ee04505118943b850
Ömer Task <omertaskesenn@gmail.com>
2025-10-16 18:05:21 +0300

    svg eklendi, tasarım iyileştirildi, kodlar temizlendi,dashboard ve privateroute sayfalarına header bağlanıldı

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import Header from '../components/Header/Header.jsx'
 
 const PrivateRoute = () => {
     return (
         <div>
+            <Header />
+
+            
             <p>PrivateRoute Component</p>
         </div>
     )
