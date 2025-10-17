c165e75934824f95aebb0f97ae32ae5a725b1603
yesimbozkurt <yesimsbozkurt@gmail.com>
2025-10-18 01:42:07 +0300

    Currency component: update table class and enhance styles for better layout

--- src/components/CurrencyChart/CurrencyChart.jsx
+++ src/components/CurrencyChart/CurrencyChart.jsx
@@ -7,12 +7,12 @@ import {
     PointElement,
     Title,
     Tooltip,
-    Filler,
+    Filler
 } from 'chart.js';
 import { Line } from 'react-chartjs-2';
-import { getFromLocalStorage } from "../../services/currencyService.js";
+import { useRef } from 'react';
 import css from './CurrencyChart.module.css';
-
+import { getFromLocalStorage } from '../../services/currencyService.js';
 
 ChartJS.register(
     CategoryScale,
@@ -25,51 +25,75 @@ ChartJS.register(
     Filler
 );
 
-const options = {
-    plugins: {
-        legend: {
-            labels: false
-        },
-        tooltip: {
-            backgroundColor: '#2c2c54',
-            titleColor: '#fff',
-            bodyColor: '#fff'
-        }
-    },
-    scales: {
-        x: {
-            ticks: { color: '#000' },
-            grid: { color: 'rgba(255,255,255,0.1)' },
-            display: false,
-
-        },
-        y: {
-            ticks: { color: '#000' },
-            grid: { color: 'rgba(255,255,255,0.1)' },
-            display: false,
-
-        }
-    }
-};
-
 export default function CurrencyChart() {
-    const USDRateData = getFromLocalStorage().data.filter((item) => (item.currencyCodeA === 840));
-    const EURRateData = getFromLocalStorage().data.filter((item) => (item.currencyCodeA === 978));
-    console.log("RateData: ", USDRateData, EURRateData);
+    const chartRef = useRef();
+
+    const USDRateData = getFromLocalStorage().data.filter((item) => item.currencyCodeA === 840);
+    const EURRateData = getFromLocalStorage().data.filter((item) => item.currencyCodeA === 978);
+
     const data = {
-        labels: ['USD - Purchase', "USD - Sale", 'EUR - Purchase', 'EUR - Sale'],
-        datasets: [{
-            data: [USDRateData[0].rateBuy, USDRateData[0].rateSell, EURRateData[0].rateBuy, EURRateData[0].rateSell],
-            borderColor: '#FF868D',
-            backgroundColor: '#39009633',
-            tension: 0.5,
-            fill: true,
-            pointRadius: 5,
-            pointHoverRadius: 7,
-            pointBackgroundColor: 'rgba(86, 62, 175, 0.5)',
-            pointBorderColor: '#FF868D'
-        },
+        labels: ['USD - Purchase', 'EUR - Purchase', 'USD - Sale', 'EUR - Sale'],
+        datasets: [
+            {
+                data: [
+                    USDRateData[0].rateBuy,
+                    EURRateData[0].rateBuy,
+                    USDRateData[0].rateSell,
+                    EURRateData[0].rateSell
+                ],
+                borderColor: '#FF868D',
+                backgroundColor: function (context) {
+                    const chart = context.chart;
+                    const { ctx, chartArea } = chart;
+                    if (!chartArea) return null;
+
+                    const gradient = ctx.createLinearGradient(0, chartArea.top, 0, chartArea.bottom);
+                    gradient.addColorStop(0, 'rgba(255,255,255,0)');
+                    gradient.addColorStop(0.2, 'rgba(255,255,255,0)');
+                    gradient.addColorStop(0.3, 'rgba(255,255,255,0.2)');
+                    gradient.addColorStop(0.5, 'rgba(255,255,255,0.4)');
+                    gradient.addColorStop(0.8, 'rgba(255,255,255,0.6)');
+                    gradient.addColorStop(1, 'rgba(255,255,255,1)');
+                    return gradient;
+                },
+                tension: 0.3,
+                fill: true,
+                pointRadius: 5,
+                pointHoverRadius: 7,
+                pointBackgroundColor: 'rgba(57, 0, 150, 0.2)',
+                pointBorderColor: '#FF868D'
+            }
         ]
     };
-    return (<div className={css.chartContainer}><Line options={options} data={data} /></div>);
+
+    const options = {
+        plugins: {
+            legend: {
+                display: false
+            },
+            tooltip: {
+                backgroundColor: '#2c2c54',
+                titleColor: '#fff',
+                bodyColor: '#fff'
+            }
+        },
+        scales: {
+            x: {
+                ticks: { color: '#000' },
+                grid: { color: 'rgba(255,255,255,0.1)' },
+                display: false
+            },
+            y: {
+                ticks: { color: '#000' },
+                grid: { color: 'rgba(255,255,255,0.1)' },
+                display: false
+            }
+        }
+    };
+
+    return (
+        <div className={css.chartContainer}>
+            <Line ref={chartRef} options={options} data={data} />
+        </div>
+    );
 }
