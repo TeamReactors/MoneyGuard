a7d228602d0aec96bdf89ba4196469e55b501172
Ömer Task <omertaskesenn@gmail.com>
2025-10-18 15:10:56 +0300

    navi first uptade

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,21 +1,39 @@
-import React from 'react';
+
 import { NavLink } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { selectIsLoggedIn } from '../../redux/auth/selectors';
+import styles from './Navigation.module.css';
 
 const Navigation = () => {
   const isLoggedIn = useSelector(selectIsLoggedIn);
+  
+  const getClasses = (isActive) => 
+    isActive ? `${styles.navLink} ${styles.active}` : styles.navLink;
 
-  // Kullanıcı giriş yapmadıysa navigasyonu gösterme şimdilik kapalı kalacak
   if (!isLoggedIn) return null;
 
   return (
-    <nav>
-      <NavLink to="/home" end>Home</NavLink>
-      <NavLink to="/statistics">Statistics</NavLink>
-      <NavLink to="/currency">Currency</NavLink>
-      
+    <nav className={styles.navigation}>
+      <NavLink to="/dashboard" className={({ isActive }) => getClasses(isActive)} end>
+        <div className={styles.linkIcon}>
+          <img src="/home.svg" alt="Home" className={styles.navIcon} />
+        </div>
+        <span className={styles.linkText}>Home</span>
+      </NavLink>
 
+      <NavLink to="/statistics" className={({ isActive }) => getClasses(isActive)}>
+        <div className={styles.linkIcon}>
+          <img src="/statistic.svg" alt="Statistics" className={styles.navIcon} />
+        </div>
+        <span className={styles.linkText}>Statistics</span>
+      </NavLink>
+
+      <NavLink to="/currency" className={({ isActive }) => getClasses(isActive)}>
+        <div className={styles.linkIcon}>
+          <img src="/dolar.svg" alt="Currency" className={styles.navIcon} />
+        </div>
+        <span className={styles.linkText}>Currency</span>
+      </NavLink>
     </nav>
   );
 };
