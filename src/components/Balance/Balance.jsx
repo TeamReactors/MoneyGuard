4dfd59334f3221a0bd85698fd7141802610a37ea
saliha <salihakamci06@gmail.com>
2025-10-19 18:20:13 +0300

    css fix

--- src/components/Balance/Balance.jsx
+++ src/components/Balance/Balance.jsx
@@ -1,11 +1,10 @@
 import { useSelector } from "react-redux";
 import { selectTransactions } from "../../redux/transactions/selectors";
-import styles from './Balance.module.css';
+import styles from "./Balance.module.css";
 import { FiDollarSign } from "react-icons/fi";
 
 const BalancePage = () => {
   const transactions = useSelector(selectTransactions);
-
   const balanceData = transactions.reduce(
     (acc, transaction) => {
       if (transaction.type === "INCOME") {
@@ -21,24 +20,22 @@ const BalancePage = () => {
   const balance = balanceData.income - balanceData.expense;
 
   return (
-       <div className={styles.containerBalance}>
+    <div className={styles.containerBalance}>
       <div className={styles.balanceCard}>
-        <h2 className={styles.titleBalance}> YOUR BALANCE
-</h2>
-       <div className={styles.balanceAmount}>
-  {balance !== null && balance !== undefined ? (
-    <>
-      <FiDollarSign/>  {balance.toLocaleString("tr-TR", {
+        <h2 className={styles.titleBalance}> YOUR BALANCE</h2>
+        <div className={styles.balanceAmount}>
+          {balance !== null && balance !== undefined ? (
+            <>
+              <FiDollarSign />{" "}
+              {balance.toLocaleString("tr-TR", {
                 minimumFractionDigits: 2,
                 maximumFractionDigits: 2,
               })}
-    </>
-  ) : (
-    'Yükleniyor...'
-  )}
-</div>
-
-       
+            </>
+          ) : (
+            "Yükleniyor..."
+          )}
+        </div>
       </div>
     </div>
   );
