0b052acac100b0f7b5fab882de6e466c65f7b740
burakezer <burakezer@outlook.com>
2025-10-24 18:45:17 +0300

    Add Statistic Page

--- src/pages/StatisticsTab.jsx
+++ src/pages/StatisticsTab.jsx
@@ -2,24 +2,11 @@ import Chart from "../components/Chart/Chart";
 import StatisticsDashboard from "../components/StatisticsDashboard/StatisticsDashboard";
 import StatisticsTable from "../components/StatisticsTable/StatisticsTable";
 import css from "../components/StatisticsDashboard/StatisticsDashboard.module.css";
-import Header from "../components/Header/Header";
-import Navigation from "../components/Navigation/Navigation";
-import Balance from "../components/Balance/Balance";
-import Currency from "../components/Currency/Currency";
 
 const StatisticsTab = () => {
   return (
     <div>
-      <Header></Header>
       <div className={css.container}>
-        <div>
-          <Navigation></Navigation>
-          <Balance></Balance>
-          <div className={css.currency}>
-            <Currency></Currency>
-          </div>
-        </div>
-
         <div className={css.area}>
           <div>
             <Chart />
